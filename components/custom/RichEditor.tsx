deabbc6d8b773de412e8f5acb9f9d3a045c5598c
carlosrobertodevops <carlos.roberto.devops@gmail.com>
2024-07-16 11:32:25 -0300

    Ajustes

--- components/custom/RichEditor.tsx
+++ components/custom/RichEditor.tsx
@@ -1,9 +1,6 @@
 "use client"
 
-// import React, { useState } from 'react';
-// import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
-// import "/components/css/quill.snow.css";
 import dynamic from "next/dynamic";
 import { useMemo } from "react";
 
