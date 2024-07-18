75405b2548ae52c6247608b6924bdbdd56bf5b99
carlosrobertodevops <carlos.roberto.devops@gmail.com>
2024-07-18 12:01:16 -0300

    Ajustes do erro: 'img.clerk.com'"

--- next.config.js
+++ next.config.js
@@ -3,19 +3,15 @@ module.exports = {
   env: {
     DATABASE_URL: process.env.DATABASE_URL,
   },
-};
-
-// next.config.js
-module.exports = {
   images: {
-    domains: ['utfs.io'],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'img.clerk.com',
-        port: '',
-        pathname: '',
-      },
-    ],
+    domains: ['utfs.io', 'img.clerk.com'],
+    // remotePatterns: [
+    //   {
+    //     protocol: 'https',
+    //     hostname: 'img.clerk.com',
+    //     port: '',
+    //     pathname: '',
+    //   },
+    // ],
   },
 };
