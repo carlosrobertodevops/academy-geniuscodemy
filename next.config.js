4ca6cc039649ed416ecff2eba1bac1eb8afb53d8
carlosrobertodevops <carlos.roberto.devops@gmail.com>
2024-07-18 19:14:23 -0300

    Ajustes nos Formulários

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 // web/next.config.js
+
+import nextI18NextConfig from './next-i18next.config.mjs';
+
 module.exports = {
+  i18n: nextI18NextConfig.i18n,
   env: {
     DATABASE_URL: process.env.DATABASE_URL,
   },
